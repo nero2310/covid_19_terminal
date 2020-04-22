89bb5d546091b5242bb0eb27994dea12c0f77bb1
nero2310 <nero2310@wp.pl>
2020-04-23 01:29:19 +0200

    Basic info about Poland

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import requests
 
 from settings import KEY
-url = "https://covid-19-data.p.rapidapi.com/docs.json"
+url = "https://covid-19-data.p.rapidapi.com/country"
+country="Poland"
+querystring = {"format":"json","name":country}
 
 headers = {
     'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
     'x-rapidapi-key':KEY
     }
 
-response = requests.request("GET", url, headers=headers)
+response = requests.request("GET", url, headers=headers,params=querystring)
 
 print(response.text)
