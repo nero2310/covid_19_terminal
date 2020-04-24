2a44a502648edfa6ebfcac649cb4c1bdf6af7966
nero2310 <nero2310@wp.pl>
2020-04-24 15:28:30 +0200

    Improvments, now today cases get today date

--- classes.py
+++ classes.py
@@ -1,8 +1,10 @@
 import requests
+import json
+from datetime import date,timedelta
 
 from settings import  API_KEY
 
-def get_list_of_countries():
+def get_list_of_countries(): # toDo rewrite save to file
 	url = "https://covid-19-data.p.rapidapi.com/help/countries"
 	query={"format":"json"}
 	headers = {
@@ -10,11 +12,18 @@ def get_list_of_countries():
     'x-rapidapi-key': API_KEY
     }
 	response=requests.request("GET",url,headers=headers,params=query)
+	json_response=json.dumps(response.json())
+	with(open("countires.json","w")) as file:
+		for element in json_response:
+			file.write(str(element))
 	return response.text
 
 
 class HeadersApi:
 	def __init__(self):
+		self.date_today=date.today()
+		self.yesterday=date.today()-timedelta(days=1)
+		self.date_today,self.yesterday=self.date_today.strftime("%Y-%m-%d"),self.yesterday.strftime("%Y-%m-%d")
 		self.headers={
 	    	'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
 	    	'x-rapidapi-key':API_KEY
@@ -39,8 +48,6 @@ class TotalCases(HeadersApi):
 class TotalCasesDaily(HeadersApi):
 	def __init__(self):
 		super().__init__()
-		self.query={"date-format":"YYYY-MM-DD","format":"json","date":"2020-04-22"}
+		self.query={"date-format":"YYYY-MM-DD","format":"json","date":self.date_today}
 		self.url="https://covid-19-data.p.rapidapi.com/report/totals"
-
-
 
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from settings import  API_KEY
 def menu():
 	print("1.Total Cases in World")
 	print("2.Total Cases in today in World")
-	print("8.Print country list")
+	print("8.Create country file")
 	print("9.Exit")
 	option=input("Chose option")
 	if option=="1":
