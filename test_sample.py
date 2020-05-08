6a95360907f248c50afc955e59a5363a3e9ce915
nero2310 <nero2310@wp.pl>
2020-05-09 01:50:12 +0200

    Fix a case validation function and add tests whose check this method

--- test_sample.py
+++ test_sample.py
@@ -8,10 +8,30 @@ json_obj=[
 		"deaths":200
 	}
 ]
+cases_dict=[
+	{
+	"confirmed":0,
+	"recovered":0
+	}
+]
 
+cases_dict_2=[
+	{
+		"confirmed":0,
+		"recovered":2
+	}
+]
+cases_dict_3=[
+	{
+		"confirmed":1,
+		"recovered":2
+	}
+]
 def test_DateAnalyzer():
 	assert classes.DateAnalyzer(json_obj).print_json()==1
-
+	assert classes.DateAnalyzer(cases_dict).cases_validity()==False
+	assert classes.DateAnalyzer(cases_dict_2).cases_validity()==True
+	assert classes.DateAnalyzer(cases_dict_3).cases_validity()==True
 
 def test_init_settings():
 	pass
