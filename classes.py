2b980bedca5cee739aed218b2a5950917da3822c
nero2310 <nero2310@wp.pl>
2020-05-10 00:33:28 +0200

    Add class whose check daily cases for a country

--- classes.py
+++ classes.py
@@ -82,4 +82,13 @@ class CasesDailyWorld(BaseApiClass, DateAnalyzer):
 		self.query = {"date-format": "YYYY-MM-DD", "format": "json", "date": self.date}
 		self.url = "https://covid-19-data.p.rapidapi.com/report/totals"
 		print(self.date)
-# class
+
+
+class CasesDailyCountry(BaseApiClass,DateAnalyzer): # toDo i must overwrite function cases_validity for this class,
+													# this class storage date in diffrent way
+	def __init__(self):
+		super().__init__()
+		self.country="Poland"
+		self.query={"date-format":"YYYY-MM-DD","format":"json","date":"2020-04-01","name":self.country}
+		self.url="https://covid-19-data.p.rapidapi.com/report/country/name"
+		print(self.date)
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import classes
 
-from settings import  API_KEY
+from settings import API_KEY
+
 
 def menu():
 	print("1.Total Cases in World")
 	print("2.Total Cases in World enter date")
 	print("8.Create country file")
 	print("9.Exit")
-	option=input("Chose option : ")
-	if option=="1":
-		data=classes.TotalCases()
+	option = input("Chose option : ")
+	if option == "1":
+		data = classes.TotalCases()
 		data.make_a_call()
-	if option=="2":
-		data=classes.CasesDailyWorld()
+	if option == "2":
+		data = classes.CasesDailyWorld()
 		data.make_a_call()
-	if option=="8":
+	if option == "3":
+		data = classes.CasesDailyCountry()
+		data.make_a_call()
+	if option == "8":
 		print(classes.get_list_of_countries())
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
 	menu()
