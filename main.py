5db8d79b014b065332ca822a42aad0bf6d6f6ab6
nero2310 <nero2310@wp.pl>
2020-05-10 18:20:53 +0200

    Little changes in main

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from settings import API_KEY
 
 def menu():
 	print("1.Total Cases in World")
-	print("2.Total Cases in World enter date")
+	print("2.Total Cases in World (user specify date)")
+	print("3 Total Cases in Country")
 	print("8.Create country file")
 	print("9.Exit")
 	option = input("Chose option : ")
