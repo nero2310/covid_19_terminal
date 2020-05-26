68de1e00425f26565ad9973002bd10974562bb4a
nero2310 <nero2310@wp.pl>
2020-05-26 13:55:37 +0200

    Format files using black

--- classes.py
+++ classes.py
@@ -7,146 +7,162 @@ from datetime import date, timedelta, datetime
 
 from settings import API_KEY
 
-if __name__=="__main__":
-	print("Don't run this file directly, run main.py instead")
+if __name__ == "__main__":
+    print("Don't run this file directly, run main.py instead")
+
 
 def get_list_of_countries():  # toDo find a way to properly format output text
-	url = "https://covid-19-data.p.rapidapi.com/help/countries"
-	query = {"format": "json"}
-	headers = {
-		'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
-		'x-rapidapi-key': API_KEY
-	}
-	response = requests.request("GET", url, headers=headers, params=query)
-	json_response = json.dumps(response.json())
-	with(open("countires.json", "w")) as file:
-		for element in json_response:
-			file.write(str(element))
-	return response.text
+    url = "https://covid-19-data.p.rapidapi.com/help/countries"
+    query = {"format": "json"}
+    headers = {
+        "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+        "x-rapidapi-key": API_KEY,
+    }
+    response = requests.request("GET", url, headers=headers, params=query)
+    json_response = json.dumps(response.json())
+    with (open("countires.json", "w")) as file:
+        for element in json_response:
+            file.write(str(element))
+    return response.text
 
 
 class DateAnalyzer:
-	def __init__(self, data):
-		self.json_obj = data
-		self.cases_dict = self.json_obj[0]
+    def __init__(self, data):
+        self.json_obj = data
+        self.cases_dict = self.json_obj[0]
 
-	def print_json(self):
-		for key, value in self.cases_dict.items():
-			print(key, value)
-		return 1
+    def print_json(self):
+        for key, value in self.cases_dict.items():
+            print(key, value)
+        return 1
 
-	def cases_validity(self):  # If active/confirmed cases are equal 0 print warning
-		if self.cases_dict.get("confirmed", 0) is False | self.cases_dict.get("recovered", 0) is False:
-			print("Warning don't have data from this date check another day")
-			print("Check day before")
-			return False
-		else:
-			return True
+    def cases_validity(self):  # If active/confirmed cases are equal 0 print warning
+        if (
+            self.cases_dict.get("confirmed", 0)
+            is False | self.cases_dict.get("recovered", 0)
+            is False
+        ):
+            print("Warning don't have data from this date check another day")
+            print("Check day before")
+            return False
+        else:
+            return True
 
 
 class BaseApiClass:
-	"""
+    """
 	Parent class only for inheritance
 	This class set default data for attributes like date_today, yesterday , headers , country
 	"""
 
-	def __init__(self):
-		self.date_today = date.today()
-		self.yesterday = date.today() - timedelta(days=1)
-		self.date_today, self.yesterday = self.date_today.strftime("%Y-%m-%d"), self.yesterday.strftime("%Y-%m-%d")
-		self.headers = {
-			'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
-			'x-rapidapi-key': API_KEY
-		}
-		self.query = {}
-		self.url = ""
-		self.date = date.today()  # Set self.date to today date
-		self.country = "Poland"  # It's only default value you should change it by call set_country method
+    def __init__(self):
+        self.date_today = date.today()
+        self.yesterday = date.today() - timedelta(days=1)
+        self.date_today, self.yesterday = (
+            self.date_today.strftime("%Y-%m-%d"),
+            self.yesterday.strftime("%Y-%m-%d"),
+        )
+        self.headers = {
+            "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+            "x-rapidapi-key": API_KEY,
+        }
+        self.query = {}
+        self.url = ""
+        self.date = date.today()  # Set self.date to today date
+        self.country = "Poland"  # It's only default value you should change it by call set_country method
 
-	def make_a_call(self):
-		'''
+    def make_a_call(self):
+        """
 		Make a request to api provider
 		:return:
 		date
-		'''
-		self.response = requests.request("GET", self.url, headers=self.headers, params=self.query)
-		date = json.loads(self.response.text)
-		return date
-
-	def set_date(self):
 		"""
+        self.response = requests.request(
+            "GET", self.url, headers=self.headers, params=self.query
+        )
+        date = json.loads(self.response.text)
+        return date
+
+    def set_date(self):
+        """
 		User set date whose will be used to make a request
 		"""
-		variable = input(
-			"Enter date example 2020-5-15 : ")  # I know it's ugly way to pass a value but i don't have idea																							# how to pass date
-		self.date = datetime.strptime(variable, "%Y-%m-%d").date()
+        variable = input(
+            "Enter date example 2020-5-15 : "
+        )  # I know it's ugly way to pass a value but i don't have idea																							# how to pass date
+        self.date = datetime.strptime(variable, "%Y-%m-%d").date()
 
-	def set_country(self):
-		"""
+    def set_country(self):
+        """
 		User set country whose will be used to make a request
 		"""
-		country = input("Enter country example Poland : ")
-		self.country = country
-		self.query = {"format": "json"}
-		self.url = "https://covid-19-data.p.rapidapi.com/totals"
+        country = input("Enter country example Poland : ")
+        self.country = country
+        self.query = {"format": "json"}
+        self.url = "https://covid-19-data.p.rapidapi.com/totals"
 
 
 class TotalCases(BaseApiClass, DateAnalyzer):
-	"""
+    """
 	Class whose is used to make a call about daily cases in world
 	"""
 
-	def __init__(self):
-		super().__init__()
-		self.url = "https://covid-19-data.p.rapidapi.com/totals"
+    def __init__(self):
+        super().__init__()
+        self.url = "https://covid-19-data.p.rapidapi.com/totals"
 
-	def print_headers(self):
-		print(self.headers)
+    def print_headers(self):
+        print(self.headers)
 
 
 class CasesDailyWorld(BaseApiClass, DateAnalyzer):
-	"""
+    """
 	Class whose is used to make a call about COVID-19 cases in world, user specify date
 	"""
 
-	def __init__(self):
-		super().__init__()
-		self.set_date()
-		self.query = {"date-format": "YYYY-MM-DD", "format": "json", "date": self.date}
-		self.url = "https://covid-19-data.p.rapidapi.com/report/totals"
+    def __init__(self):
+        super().__init__()
+        self.set_date()
+        self.query = {"date-format": "YYYY-MM-DD", "format": "json", "date": self.date}
+        self.url = "https://covid-19-data.p.rapidapi.com/report/totals"
 
 
-class CasesDailyCountry(BaseApiClass, DateAnalyzer):  # toDo i must overwrite function cases_validity for this class,
-	# this class storage date in diffrent way
-	"""
+class CasesDailyCountry(
+    BaseApiClass, DateAnalyzer
+):  # toDo i must overwrite function cases_validity for this class,
+    # this class storage date in diffrent way
+    """
 	Class whose is used to make a call about COVID-19 cases in country, user specify country and date
 	"""
 
-	def __init__(self):
-		super().__init__()
-		self.set_date()
-		self.set_country()
-		self.query = {"date-format": "YYYY-MM-DD", "format": "json", "date": self.date, "name": self.country}
-		self.url = "https://covid-19-data.p.rapidapi.com/report/country/name"
+    def __init__(self):
+        super().__init__()
+        self.set_date()
+        self.set_country()
+        self.query = {
+            "date-format": "YYYY-MM-DD",
+            "format": "json",
+            "date": self.date,
+            "name": self.country,
+        }
+        self.url = "https://covid-19-data.p.rapidapi.com/report/country/name"
 
 
 class PandasDataAnalyzer:
-	def __init__(self,data):
-		index_key=data[0].pop("lastChange")
-		self.data_frame=pd.DataFrame(data,index=[index_key])
-		print(self.data_frame)
+    def __init__(self, data):
+        index_key = data[0].pop("lastChange")
+        self.data_frame = pd.DataFrame(data, index=[index_key])
+        print(self.data_frame)
 
-	def save_to_json(self,file_name="database.json"):
-		if not os.path.isfile(file_name):
-			try:
-				self.data_frame.to_json(path_or_buf=file_name)
-			except:
-				pass
+    def save_to_json(self, file_name="database.json"):
+        if not os.path.isfile(file_name):
+            try:
+                self.data_frame.to_json(path_or_buf=file_name)
+            except:
+                pass
 
+    def load_from_json(self):
+        pass
 
-	def load_from_json(self):
-		pass
-
-	def print_data_frame(self):
-		pass
-
+    def print_data_frame(self):
+        pass
