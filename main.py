3021d0b4aa55980866ec9e1972a729a4c976c304
nero2310 <nero2310@wp.pl>
2020-06-18 22:31:29 +0200

    Create directory for test and move test to it

--- main.py
+++ main.py
@@ -17,18 +17,26 @@ def menu():
         data_analyzer = classes.DateAnalyzer(python_object).print_json()
     if option == "2":
         data = classes.CasesDailyWorld()
-        data.set_date(input("Set date from which cases will be displayed example 2020-5-15 "))
+        data.set_date(
+            input("Set date from which cases will be displayed example 2020-5-15 ")
+        )
         python_object = data.make_a_call()
         data_analyzer = classes.DateAnalyzer(python_object).print_json()
     if option == "3":
         data = classes.CasesDailyCountry()
-        data.set_date(input("Set date from which cases will be displayed example 2020-5-15 "))
+        data.set_date(
+            input("Set date from which cases will be displayed example 2020-5-15 ")
+        )
         python_object = data.make_a_call()
-        each_or_summary=input("Do you wanna see cases in country or cases in regions like states etc.?")
-        if each_or_summary=="1" or each_or_summary=="regions":
+        each_or_summary = input(
+            "Do you wanna see cases in country or cases in regions like states etc.?"
+        )
+        if each_or_summary == "1" or each_or_summary == "regions":
             data_analyzer = classes.DateAnalyzer(python_object).print_provinces()
-        elif  each_or_summary=="2" or each_or_summary=="country":
-            data_analyzer = classes.DateAnalyzer(python_object).print_json() #toDo  output the data in a condensed form
+        elif each_or_summary == "2" or each_or_summary == "country":
+            data_analyzer = classes.DateAnalyzer(
+                python_object
+            ).print_json()  # toDo  output the data in a condensed form
 
     if option == "4":
         data = classes.CasesInWorld()
@@ -40,12 +48,13 @@ def menu():
     if option == "8":
         print(classes.get_list_of_countries())
 
-    if option =="9":
+    if option == "9":
         return False
 
     return True
 
+
 if __name__ == "__main__":
-    loop=True
+    loop = True
     while loop:
-        loop=menu()
+        loop = menu()
