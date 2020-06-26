d8a6a1d8174173029fbba598bd8c1bfa6a36a8ec
nero2310 <nero2310@wp.pl>
2020-06-26 14:51:20 +0200

    Move class responsible for connection with database to new file

---
+++ classes/database_connector.py
@@ -0,0 +1,51 @@
+import pymongo
+
+from pymongo import errors
+
+
+class SaveDataToMongo:
+    """
+    Initialize connection and save data to mongoDB
+    """
+
+
+def __init__(self):  # toDO check if connection is valid
+    self.colection = None
+
+
+def init_connection(self):
+    """
+    Initialize connection to mongodb
+    :return:
+    True if connection succeed
+    False if connection didn't succeed
+    """
+    try:
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        mydb = client["covid_19_db"]
+        self.colection = mydb["test"]
+    except pymongo.errors.ConnectionFailure:
+        return False
+    except pymongo.errors.ServerSelectionTimeoutError:
+        return False
+    return True
+
+
+def insert_data(self, data: dict):
+    """
+    Method send data to mongodb
+    If data is dict
+    Else raise TypeError
+    :param self:
+    :param data:dict:
+    :return:
+    """
+    try:
+        if isinstance(data, dict):
+            self.colection.insert_one(data)
+        elif isinstance(data[0], dict):
+            self.colection.insert_one(data[0])
+        else:
+            raise TypeError
+    except pymongo.errors.ServerSelectionTimeoutError:
+        print("Can't connect to server")
