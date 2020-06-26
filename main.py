c1d14c23e026158886045ebee8622a8dd5f00bd1
nero2310 <nero2310@wp.pl>
2020-06-26 14:32:31 +0200

    Rewrite menu

--- main.py
+++ main.py
@@ -2,26 +2,28 @@ from classes import classes
 
 
 def menu():
-    print("1.Total Cases in World")
-    print("2.Total Cases in World (user specify date)")
-    print("3 Total Cases in Country (user specify date)")
-    print("4.TESTING send data about total cases to pandas dataframe")
-    print("5.TESTING send data to MongoDB")
-    print("8.Create country file")
-    print("9.Exit")
-    option = input("Chose option : ")
-    if option == "1":
+    choice=int(input('''
+Main Menu
+1.Total Cases in World
+2.Total Cases in World (user specify date)
+3 Total Cases in Country (user specify date)
+4.TESTING send data about total cases to pandas dataframe
+5.TESTING send data to MongoDB
+8.Create country file
+9.Exit 
+:'''))
+    if choice == 1:
         data = classes.CasesInWorld()
         json_object = data.make_a_call()
         data_analyzer = classes.DateAnalyzer(json_object).print_json()
-    if option == "2":
+    if choice == 2:
         data = classes.CasesDailyWorld()
         data.set_date(
             input("Set date from which cases will be displayed example 2020-5-15 ")
         )
         json_object = data.make_a_call()
         data_analyzer = classes.DateAnalyzer(json_object).print_json()
-    if option == "3":
+    if choice == 3:
         data = classes.CasesDailyCountry()
         data.set_date(
             input("Set date from which cases will be displayed example 2020-5-15 ")
@@ -37,14 +39,14 @@ def menu():
                 json_object
             ).print_json()  # toDo  output the data in a condensed form
 
-    if option == "4":
+    if choice == 4:
         data = classes.CasesInWorld()
         json_object = data.make_a_call()
         dataFrame = classes.PandasDataAnalyzer(json_object)
         dataFrame.print_data_frame()
         dataFrame.save_to_json("example.json")
 
-    if option == "5":
+    if choice == 5:
         data = classes.CasesInWorld()
         json_object = data.make_a_call()
         connection = classes.SaveDataToMongo()
@@ -54,10 +56,10 @@ def menu():
         else:
             print("Connection Failure")
 
-    if option == "8":
+    if choice == 8:
         print(classes.get_list_of_countries())
 
-    if option == "9":
+    if choice == 9:
         return False
 
     return True
